3c5108cc1dce32c35228a55a948d299404ba97a7
Kry9toN <kry9ton@kryptonproject.my.id>
2024-06-05 22:42:27 +0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by KryPtoN on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
